86ae9ccf2ed9df32ef44e92d3fea1f84626e5e53
mattia richetto <mattia.richetto@gmail.com>
2016-01-08 09:38:54 +0000

    add kilometers to mile

--- public/javascripts/pace-calculator/pace-calculator.controller.js
+++ public/javascripts/pace-calculator/pace-calculator.controller.js
@@ -2,11 +2,15 @@ function PaceCalculatorController($http, $log) {
     'use strict';
 
     var vm = this;
-    vm.toKph = toKph
+    vm.toSpeed = toSpeed;
+
+    vm.kilometers = 9;
+    vm.toMiles = toMiles;
+    toMiles();
 
     $http.get('/pace-calculator').then(function successCallback(response) {
         setTime(response.data);
-        toKph();
+        toSpeed();
     }, errorCallback);
 
     function setTime(data) {
@@ -14,14 +18,25 @@ function PaceCalculatorController($http, $log) {
         vm.seconds = data.seconds;
     }
 
-    function toKph() {
+    function toSpeed() {
         var kph = 60 / (vm.minutes + (vm.seconds / 60));
+        var mph = kph * 0.621371;
+
         vm.kph = Math.round(kph * 100) / 100;
-        $log.info('minutes: [' + vm.minutes + '] seconds: [' + vm.seconds + '] kph: [' + vm.kph + ']');
+        vm.mph = Math.round(mph * 100) / 100;
+
+        $log.info('minutes: [' + vm.minutes + '] seconds: [' + vm.seconds + '] kph: [' + vm.kph + '] mph: [' + vm.mph + ']');
 
         $http.post('/pace-calculator', { paceCalculatorDefaults: { minutes: vm.minutes, seconds: vm.seconds } }).then(function successCallback(response) {
             setTime(response.data);
         }, errorCallback);
+    }
+
+    function toMiles() {
+        var miles = vm.kilometers * 0.621371;
+        vm.miles = Math.round(miles * 100) / 100;
+        
+        $log.info('kilometers: [' + vm.kilometers + '] miles: [' + vm.miles + ']');
     }
 
     function errorCallback(response) {
