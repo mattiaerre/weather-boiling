6809d897e15b318efc9796cb942888c694061703
mattia richetto <mattia.richetto@gmail.com>
2016-05-31 23:00:58 +0100

    surface version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var version = require('../package.json').version;
 
 router.get('/', function (req, res, next) {
   //res.redirect(302, '/rxjs');
-  res.render('index', { title: 'weather-boiling' });
+  res.render('index', { title: 'weather-boiling', version: 'v' + version });
 });
 
 module.exports = router;
