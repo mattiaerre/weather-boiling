9a375a503c3a2410c247c5b129e024e978e8b6dd
mattia richetto <mattia.richetto@gmail.com>
2016-05-31 23:35:52 +0100

    housekeeping

--- public/javascripts/app.module.js
+++ public/javascripts/app.module.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    var appName = 'weather-boiling-app'
+    var appName = 'weather-boiling-app';
     angular.module(appName, [])
         .constant('_', window._)
         .constant('moment', window.moment); // see: http://stackoverflow.com/questions/23862119/how-to-make-lodash-work-with-angular-js
