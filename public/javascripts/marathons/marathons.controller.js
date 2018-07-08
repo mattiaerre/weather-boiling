491a38e137070a319e722a5d719538be3ab635b2
mattia richetto <mattia.richetto@gmail.com>
2018-07-08 21:40:48 +0200

    add moscow

--- public/javascripts/marathons/marathons.controller.js
+++ public/javascripts/marathons/marathons.controller.js
@@ -1,39 +1,46 @@
 function MarathonsController($http, _, moment, $log) {
-    'use strict';
+  'use strict';
 
-    var vm = this;
-    vm.marathons = [];
-    vm.getWeather = getWeather;
+  var vm = this;
+  vm.marathons = [];
+  vm.getWeather = getWeather;
 
-    $http.get('/marathons').then(function successCallback(response) {
-        var marathons = [];
+  $http.get('/marathons').then(function successCallback(response) {
+    var marathons = [];
 
-        _.each(response.data, function (item) { // todo: name this function
-            var when = moment(item.when, 'DD/MM/YYYY');
-            // mapping
-            marathons.push({
-                id: item.id,
-                fromNow: when.fromNow(),
-                unix: when.unix(),
-                future: when.isAfter(moment()),
-                format: when.format('dddd, MMMM Do YYYY'),
-                city: item.info.city
-            });
-        });
+    _.each(response.data, function(item) {
+      // todo: name this function
+      var when = moment(item.when, 'DD/MM/YYYY');
+      // mapping
+      marathons.push({
+        id: item.id,
+        fromNow: when.fromNow(),
+        unix: when.unix(),
+        future: when.isAfter(moment()),
+        format: when.format('dddd, MMMM Do YYYY'),
+        city: item.info.city
+      });
+    });
 
-        // sorting
-        vm.marathons = _.sortByOrder(marathons, ['unix'], ['desc']);
+    // sorting
+    vm.marathons = _.sortByOrder(marathons, ['unix'], ['desc']);
+  }, errorCallback);
+
+  function errorCallback(response) {
+    $log.log('error');
+    $log.log(response);
+  }
+
+  function getWeather(marathon) {
+    const uri = encodeURI(`/weather?q=${marathon.city}`);
+    $http.get(uri).then(function successCallback(response) {
+      var weather = response.data.weather;
+      vm.weather = {
+        city: weather.name,
+        description: weather.weather[0].main,
+        icon: weather.weather[0].icon,
+        temp: weather.main.temp
+      };
     }, errorCallback);
-
-    function errorCallback(response) {
-        $log.log('error');
-        $log.log(response);
-    }
-
-    function getWeather(marathon) {
-        $http.get('/weather?q=' + marathon.city).then(function successCallback(response) {
-            var weather = response.data.weather;
-            vm.weather = { city: weather.name, description: weather.weather[0].main, icon: weather.weather[0].icon, temp: weather.main.temp };
-        }, errorCallback);
-    }
+  }
 }
--- routes/weather.js
+++ routes/weather.js
@@ -4,26 +4,33 @@ var router = express.Router();
 
 var apiKey = process.env.OPENWEATHERMAPORG_APIKEY;
 
-router.get('/', function (req, res, next) {
-    getWeather(req.query.q, function (weather) {
-        res.send({ weather: weather });
-    }); // info: not sure about this
+router.get('/', function(req, res, next) {
+  getWeather(req.query.q, function(weather) {
+    res.send({ weather: weather });
+  }); // info: not sure about this
 });
 
 function getWeather(q, callback) {
-    return http.get({
-        host: 'api.openweathermap.org',
-        path: '/data/2.5/weather?q=' + q + '&appid=' + apiKey + '&units=metric' // see: http://openweathermap.org/current#name
-    }, function (response) {
-        var body = '';
-        response.on('data', function (d) {
-            body += d;
-        });
-        response.on('end', function () {
-            var parsed = JSON.parse(body);
-            callback(parsed);
-        });
-    });
+  const path = encodeURI(
+    `/data/2.5/weather?q=${q}&appid=${apiKey}&units=metric`
+  );
+
+  return http.get(
+    {
+      host: 'api.openweathermap.org',
+      path // see: http://openweathermap.org/current#name
+    },
+    function(response) {
+      var body = '';
+      response.on('data', function(d) {
+        body += d;
+      });
+      response.on('end', function() {
+        var parsed = JSON.parse(body);
+        callback(parsed);
+      });
+    }
+  );
 }
 
 module.exports = router;
